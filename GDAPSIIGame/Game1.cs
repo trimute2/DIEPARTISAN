156760bce52be551a2f0fd5cf21cde2d14502558
jpa3216 <jpa3216@GDD-57>
2017-01-30 18:10:44 -0500

    Updated the Game1 Class.

--- GDAPSIIGame/Game1.cs
+++ GDAPSIIGame/Game1.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using FarseerPhysics.Dynamics;
-using FarseerPhysics.Collision;
-using FarseerPhysics.Factories;
 
 namespace GDAPSIIGame
 {
