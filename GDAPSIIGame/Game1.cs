5480416a4d7ad23395d5547e6bcaf4f23823d902
bjs2904 <bjs2904@rit.edu>
2017-02-01 17:55:52 -0500

    added controls

--- GDAPSIIGame/Game1.cs
+++ GDAPSIIGame/Game1.cs
@@ -6,10 +6,14 @@ namespace GDAPSIIGame
 {
     public class Game1 : Game
     {
+		GameObject player;
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+		Texture2D playerTexture;
+		KeyboardState kbState;
+		KeyboardState previousKbState;
 
-        public Game1()
+		public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
@@ -17,12 +21,16 @@ namespace GDAPSIIGame
 
         protected override void Initialize()
         {
-            base.Initialize();
+			player = new GameObject(playerTexture,new Rectangle(0,0,5,5)); 
+			kbState = new KeyboardState();
+			previousKbState = kbState;
+			base.Initialize();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+			playerTexture = Content.Load<Texture2D>("player");
         }
 
         protected override void UnloadContent()
@@ -35,7 +43,25 @@ namespace GDAPSIIGame
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            base.Update(gameTime);
+			if (kbState.IsKeyDown(Keys.W) || kbState.IsKeyDown(Keys.Up))
+			{
+				player.Y -= 5;
+			}
+			else if (kbState.IsKeyDown(Keys.S) || kbState.IsKeyDown(Keys.Down))
+			{
+				player.Y += 5;
+			}
+
+			if (kbState.IsKeyDown(Keys.D) || kbState.IsKeyDown(Keys.Right))
+			{
+				player.X += 5;
+			}
+			else if (kbState.IsKeyDown(Keys.A) || kbState.IsKeyDown(Keys.Left))
+			{
+				player.X -= 5;
+			}
+
+			base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
