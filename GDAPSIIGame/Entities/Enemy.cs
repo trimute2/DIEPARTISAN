fe666925d2609850cd8e707018596f675a6b1b39
dsg6874 <dsg6874@g.rit.edu>
2017-02-01 18:19:55 -0500

    Added Enemy Class.

---
+++ GDAPSIIGame/Entities/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GDAPSIIGame.Entities
+{
+    class Enemy : Entity
+    {
+        public Enemy(int health, int moveSpeed, Texture2D texture, Rectangle position) : base(health, moveSpeed, texture, position)
+        {
+        }
+
+        public void Move(GameObject thingToMoveTo)
+        {
+            
+        } 
+    }
+}
