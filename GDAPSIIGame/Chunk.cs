14d72bd10cd343209759bd764e4766c704936d15
bjs2904 <bjs2904@NM2550-18>
2017-02-03 10:47:29 -0500

    started a chunk class

---
+++ GDAPSIIGame/Chunk.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace GDAPSIIGame
+{
+	class Chunk
+	{
+		private List<GameObject> obj;
+		Rectangle area;
+
+		public List<GameObject> Objects
+		{
+			get { return obj; }
+		}
+
+		public Chunk(Rectangle area)
+		{
+			this.area = area;
+			obj = new List<GameObject>();
+		}
+
+		public bool Contains(Vector2 pos)
+		{
+			return area.Contains(pos);
+		}
+		
+	}
+}
