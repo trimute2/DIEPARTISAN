efb20e97785a34153bc448fd74c771385ceaa421
bjs2904 <bjs2904@GDD-36>
2017-05-08 18:34:20 -0400

    skelton of menus

---
+++ GDAPSIIGame/Button.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GDAPSIIGame
+{
+    class Button
+    {
+
+        private Texture2D texture;
+        private Rectangle area;
+		private bool selected;
+        private string text;
+
+		public bool Selected
+		{
+			set { selected = value; }
+		}
+
+
+        public Button(Texture2D texture, Rectangle area, string text)
+        {
+            this.texture = texture;
+            this.area = area;
+            this.text = text;
+			selected = false;
+        }
+        
+        public bool Contains(Vector2 pos)
+        {
+            return area.Contains(pos);
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(texture, area, Color.White);
+            sb.DrawString(TextureManager.Instance.GetFont("uifont"),text,area.Center.ToVector2(),Color.MediumSeaGreen);
+        }
+    }
+}
