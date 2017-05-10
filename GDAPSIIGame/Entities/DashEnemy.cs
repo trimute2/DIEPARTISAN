436bde4e9415446d4d0ab6f9c4b7b8f490bd4383
bjs2904 <ben.j.stern@gmail.com>
2017-05-09 21:35:00 -0400

    somewhat of a fix for dash enemies

--- GDAPSIIGame/Entities/DashEnemy.cs
+++ GDAPSIIGame/Entities/DashEnemy.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using GDAPSIIGame.Interface;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using GDAPSIIGame.Graph;
 
 namespace GDAPSIIGame.Entities
 {
@@ -17,6 +18,10 @@ namespace GDAPSIIGame.Entities
 		private bool bump;
 		private float bumpTime;
 		private float dashSpeed;
+
+		public Vector2 CurrentTarget { get; set; }
+
+		public List<Vector2> RecentTargets { get; set; }
 
 		public float DashTime
 		{
@@ -35,6 +40,8 @@ namespace GDAPSIIGame.Entities
 			dashing = false;
 			dashSpeed = 8f;
 			color = Color.Red;
+			RecentTargets = new List<Vector2>();
+			CurrentTarget = Vector2.Zero;
 			bump = false;
 		}
 
@@ -45,12 +52,38 @@ namespace GDAPSIIGame.Entities
 			dashing = false;
 			dashSpeed = 4f;
 			color = Color.Red;
+			RecentTargets = new List<Vector2>();
+			CurrentTarget = Vector2.Zero;
 			bump = false;
 		}
 
 		public override void Update(GameTime gameTime)
 		{
 			previousPosition = Position;
+			if (Awake)
+			{
+				if (CurrentTarget == Vector2.Zero || Vector2.Distance(CurrentTarget, Position) < 5)
+				{
+					//Console.WriteLine("Close");
+					/*
+                    if (Path.Count > 0)
+                    {
+                        CurrentTarget = Path[0].Position;
+                        Path.RemoveAt(0);
+                        Console.WriteLine("Next");
+                    }*/
+					//else
+					//{
+					//CurrentTarget = Player.Instance.Position;
+					FindNextTarget(Player.Instance.Position, Graph.Graph.Instance);
+					//}
+				}
+				else
+				{
+					//Console.WriteLine("New paf + {0}", Position.X);
+					//FindNextTarget(Player.Instance.Position, Graph.Graph.Instance);
+				}
+			}
 			if (!Awake)
 			{
 				if (Vector2.Distance(
@@ -189,5 +222,51 @@ namespace GDAPSIIGame.Entities
 			}
 			base.OnCollision(obj);
         }
-    }
+
+		public void FindNextTarget(Vector2 position, Graph.Graph graph)
+		{
+			//Find closest Node to self and goal
+			GraphNode closestToGoal = graph.FindClosestNode(position);
+			GraphNode closestToMe = graph.FindClosestNode(this.Position);
+			float currDistance = float.MaxValue;
+
+			GraphNode currNode = closestToGoal;
+			/*
+            GraphNode prevNode = closestToMe;
+            while (currNode != closestToMe && closestToMe.Neighbors[currNode] != currNode)
+            {
+                prevNode = currNode;
+                currNode = closestToMe.Neighbors[currNode];
+            }
+            */
+
+			//Find closest Node to player from neighbors
+			foreach (GraphNode node in closestToMe.Neighbors.Values)
+			{
+				if (!RecentTargets.Contains(node.Position))
+				{
+					float thisDistance = Vector2.Distance(node.Position, closestToGoal.Position);
+					if (thisDistance < currDistance)
+					{
+						currNode = node;
+						currDistance = thisDistance;
+					}
+				}
+			}
+
+			//Keep track of recent nodes so we don't loop
+			while (RecentTargets.Count > 3)
+			{
+				RecentTargets.RemoveAt(0);
+			}
+			//path.Reverse();
+			//path.Insert(0, closestToMe);
+			//showPath();
+
+			//Set target and add to recenttargets so we don't move backwards
+			CurrentTarget = currNode.Position;
+			RecentTargets.Add(CurrentTarget);
+		}
+
+	}
 }
